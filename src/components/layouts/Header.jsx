94d2695c97fe49182944be3618f8dfe325c29c47
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-18 09:39:43 +0700

    change for firs build

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Navbar</span>
+      <span className="navbar-brand mb-0 h1">Long PK</span>
       <div>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
       </div>
