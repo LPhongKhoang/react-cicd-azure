715c12a2319799cdbcd555ad65f818326f512ba5
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-18 10:03:00 +0700

    change logo

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Long PK</span>
+      <span className="navbar-brand mb-0 h1">Long PK pro</span>
       <div>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
       </div>
